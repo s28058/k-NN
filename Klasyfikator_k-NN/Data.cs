2f1f487652cd5a13394ba1776f8090b47b8cbb2f
s28058 <s28058@pjwstk.edu.pl>
2024-03-14 21:46:08 +0100

    czytanie danych

---
+++ Klasyfikator_k-NN/Data.cs
@@ -0,0 +1,7 @@
+namespace Klasyfikator_k_NN;
+
+public class Data
+{
+    public double[] Values { get; set; }
+    public string Label { get; set; }
+}
---
+++ Klasyfikator_k-NN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using Klasyfikator_k_NN;
+
+var trainingData = ReadFile("Data/trainSet.csv");
+
+Console.WriteLine("Training file loaded");
+
+static List<Data> ReadFile(string fileName)
+{
+    var lines = File.ReadLines(fileName);
+    List<Data> result = new List<Data>();
+    foreach (var line in lines)
+    {
+        var elements = line.Split(";");
+        var label = elements.Last();
+        var values = elements[..^1]
+            .Select(v => double.Parse(v, CultureInfo.InvariantCulture))
+            .ToArray();
+        Data data = new Data() { Values = values, Label = label };
+        result.Add(data);
+    }
+
+    return result;
+}
